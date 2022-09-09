0dca68e73b2ced5ae6f15bcbc880854be8928821
Ana Cecilia Souza <anaceciams@gmail.com>
2022-09-09 16:23:41 -0300

    arquivos alterados

---
+++ telas/telaInicial.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+const width = Dimensions.get('screen').width;
+
+
+export default function TelaInicial() {
+  return (
+    <View style={estilos.PosicaoTitulo}>
+      <Text style = {estilos.titulo}> Security For You </Text>
+      
+      <TouchableOpacity style={estilos.botao}>
+      <Text style={estilos.textoBotao}> Enviar SMS </Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={estilos.botao}>
+      <Text style={estilos.textoBotao}> Gravar ligação </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={estilos.botao}>
+      <Text style={estilos.textoBotaoSOS}> Pedir ajuda </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={estilos.botao}>
+      <Text style={estilos.textoBotao}> Configurações </Text>
+      </TouchableOpacity>
+
+      
+    </View>
+
+  );
+}
+
+const estilos = StyleSheet.create({
+  PosicaoTitulo: {
+    width: "100%",
+    position: "absolute",
+  },
+  
+  titulo:{
+    marginTop: 10,
+    textAlign:"center",
+    color:"#464646",
+    fontWeight: "bold",
+    fontSize:26,
+    lineHeight:42,
+    padding:16,    
+  }, 
+  botao: {
+    backgroundColor: "#F0F0F0",
+    paddingVertical: 16,
+    borderRadius: 6,
+    marginTop: 250,
+    marginLeft:10,
+    marginRight: 10,
+  },
+  textoBotao: {
+    textAlign: "center",
+    color: "#000000",
+    fontSize: 20,
+    lineHeight: 26,
+    fontWeight: "bold",
+  },
+  textoBotaoSOS: {
+    textAlign: "center",
+    color: "#FF0000",
+    fontSize: 20,
+    lineHeight: 26,
+    fontWeight: "bold",
+  }
+});
+
+
+
+
+
+/*import react from "react";
+import Botao from "../componentes/botão";
+import { StyleSheet, Text, View, Button} from 'react-native';
+
+
+export default function TelaInicial()
+{
+    return (
+        <View> 
+            <button>
+            <Text style={estilos.textoBotao}> UYUSGDDASVDULAB </Text>
+
+            </button>
+        </View>
+    )
+}*/
